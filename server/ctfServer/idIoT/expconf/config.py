def130ff487482abb6c1f90bfe2691ea7f67fcfb
root <root@localhost.localdomain>
2017-06-26 11:05:34 +0000

    implement exploits configuration module

--- server/ctfServer/idIoT/expconf/config.py
+++ server/ctfServer/idIoT/expconf/config.py
@@ -1,7 +1,39 @@
-class Config:
-    """A simple example class"""
-    i = 12345
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-    def f(self):
-        return 'hello world'
+import json
+from pathlib import Path
 
+class ExploitConfig:
+	
+	path = 'exploits.config.json'
+	exploits = {}
+
+	def __init__(self):
+		self.read()
+
+	def get(self, exploit, key):
+		if exploit in self.exploits and key in self.exploits[exploit]:
+			return self.exploits[exploit][key]
+		else:
+			return '';
+
+	def put(self, exploit, key, value):
+		if exploit not in self.exploits:
+			self.exploits[exploit] = {}
+
+		self.exploits[exploit][key] = value
+		self.save()
+
+	def getAll(self):
+		return self.exploits.items()
+
+	def save(self):
+		with open(self.path, 'w') as outfile:
+		    json.dump(self.exploits, outfile)
+	
+	def read(self):
+		conf_file = Path(self.path)
+		if conf_file.is_file():
+			with open(self.path) as json_data_file:
+			    self.exploits = json.load(json_data_file)
