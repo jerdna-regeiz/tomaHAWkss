3aa4a47a04b5d48e7802e3010577f4d1513ecae9
lilli <caothivananh98@gmail.com>
2017-06-26 14:15:52 +0200

    add rest schnittstelle

--- server/ctfServer/idIoT/server.py
+++ server/ctfServer/idIoT/server.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask
-
+from flask import Response
+from flask import request
 from . import config
 
 app = Flask(__name__)
 for blue in config.blueprints:
     app.register_blueprint(blue)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+@app.route('/exploit/<name>/<period>', methods=["POST","DELETE"])
+def exploit(name, period) :
+	if request.method =='POST': 
+#Exploit with <name> and <period> should be excuted after scheduling here
+#Exploit should be in the file with <name> in the same folder
+
+		resp=Response("post request")
+		resp.headers['Access-Control-Allow-Origin'] ='*'
+		return resp
+	else: 
+
+#Exploit with name = <name> should be stopped here
+		resp=Response("delete request")
+		resp.headers['Access-Control-Allow-Origin'] ='*'
+		return resp
+	
+@app.route('/runMonitor', methods=["POST"])
+def monitor():
+#Monitoring should be called here and response to the gui
+
+	resp=Response("post request")
+	return resp
+
+@app.route('/addTarget', methods=["POST"])
+def target() :
+#Target should be added and saved here
+	resp=Response("post request")
+	return resp	
 
 def run():
     port = 4567
