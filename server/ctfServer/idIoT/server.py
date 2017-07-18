3ded46525d89e031a0d16e28f09355a1c1996d3c
Alex <aleksandr.sultanov@haw-hamburg.de>
2017-07-19 00:05:32 +0200

    fix server.py

--- server/ctfServer/idIoT/server.py
+++ server/ctfServer/idIoT/server.py
@@ -70,7 +70,7 @@ def exploitDelete(file_name):
 	if request.method == 'DELETE':
 		os.remove(os.path.join(app.config['UPLOAD_FOLDER'], file_name))
 		local = salt.client.LocalClient()
-		local.cmd('*', 'cmd.run', ['salt "*" cmd.run "rm ~/lib/' + file_name + '"'])
+		local.cmd('*', 'cmd.run', ['rm ~/lib/' + file_name])
 		#subprocess.call('salt "*" cmd.run "rm ~/lib/' + file_name + '"', shell=True)
 		return 'succesfully deleted ' + file_name
 
